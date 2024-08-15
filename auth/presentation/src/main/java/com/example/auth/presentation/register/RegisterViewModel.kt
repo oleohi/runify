767d05d9ac941620e83f05ae0678b2c9b316fc8f
olohialli <oleohi.alli@fcmb.com>
2024-08-15 22:52:01 +0100

    Register screen and user validation logic

---
+++ auth/presentation/src/main/java/com/example/auth/presentation/register/RegisterViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.auth.presentation.register
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.auth.domain.UserDataValidator
+
+class RegisterViewModel(
+    private val userDataValidator: UserDataValidator
+): ViewModel(){
+    var state by mutableStateOf(RegisterState)
+        private set
+
+    init {
+        state.email.textAsFlow()
+            .onEach { email ->
+                state = state.copy(
+                    isEmailValid = userDataValidator.isValidEmail(email.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+
+        state.password.textAsFlow()
+            .onEach { password ->
+                state = state.copy(
+                    passwordValidationState = userDataValidator.validatePassword(password.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+    }
+
+    fun onAction(action: RegisterAction) {
+
+    }
+}
